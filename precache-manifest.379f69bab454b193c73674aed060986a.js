88d2cdefb08cce4f81549cd4893a1f3c9f6939b7
Vignesh <mailtovigneshmanickam@gmail.com>
2021-05-20 14:29:33 +0530

    Updates

---
+++ precache-manifest.379f69bab454b193c73674aed060986a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d887499f97cbec8a7dcd3a10f944ed",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "72026ee4a69a8daf412b",
+    "url": "/notes/static/js/2.97854f3c.chunk.js"
+  },
+  {
+    "revision": "72e5170d6d23e0955eb7",
+    "url": "/notes/static/js/main.962678f7.chunk.js"
+  },
+  {
+    "revision": "c0dda29e67586c076e82",
+    "url": "/notes/static/js/runtime-main.08826494.js"
+  },
+  {
+    "revision": "93eed53a0a81ac48f8650796cf1eb912",
+    "url": "/notes/static/media/1.93eed53a.png"
+  },
+  {
+    "revision": "cf7af3ae86a81f36e66d55eb51231c7e",
+    "url": "/notes/static/media/think.cf7af3ae.gif"
+  },
+  {
+    "revision": "300667d0b952ec8311181010d850b748",
+    "url": "/notes/static/media/title1.300667d0.png"
+  }
+]);
